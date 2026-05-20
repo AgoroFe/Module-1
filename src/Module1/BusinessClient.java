90304124045da945a5a29262ca456b384f13d12a
AgoroFe <omondifelix275@gmail.com>
2026-05-20 16:31:11 +0200

    added attributes to the businessClient object.

--- src/Module1/BusinessClient.java
+++ src/Module1/BusinessClient.java
@@ -1,4 +1,24 @@
 package Module1;
 
 public class BusinessClient {
+    private String businessClientId;
+    private String businessType;
+    private String pickupLocation;
+    private boolean loggedIn = false;
+
+    public BusinessClient(String businessClientId, String businessType, String pickupLocation, boolean loggedIn) {
+        this.businessClientId = businessClientId;
+        this.businessType = businessType;
+        this.pickupLocation = pickupLocation;
+        this.loggedIn = loggedIn;
+    }
+
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
 }
